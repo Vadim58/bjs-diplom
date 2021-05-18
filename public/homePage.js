cbfcd232d6420007940b6a6d16d3fe69d34153ac
Vadim58 <awesome.rdeg@yandex.ru>
2021-05-18 20:36:57 +0300

    Keep learning

--- public/homePage.js
+++ public/homePage.js
@@ -1,7 +1,36 @@
 const logoutButton = new LogoutButton();
-
 logoutButton.action = () => ApiConnector.logout( function callback(){
  return location.reload();	
 })
 
+ApiConnector.current(function callback(response) {
+	if(response.success === true) {
+      return ProfileWidget.showProfile(response.data)
+	}
+})
 
+const ratesBoard = new RatesBoard();
+function getCurrencyRates () {
+	return ApiConnector.getStocks(function callback(response) {
+		if(response.success === true) {
+			 ratesBoard.clearTable();
+             ratesBoard.fillTable(response.data);
+		}
+	})
+}
+getCurrencyRates(ratesBoard);
+setInterval(getCurrencyRates, 6000);
+
+const moneyManager = new MoneyManager();
+moneyManager.addMoneyCallback = (data) => getAddMoney(data);
+function getAddMoney(data) {
+         return ApiConnector.addMoney(data, function callback(response) {
+            if(response.success === true) {            
+            	ProfileWidget.showProfile(response.data);
+            	return moneyManager.setMessage(response.data);
+            } 
+            else {
+                  return moneyManager.setMessage(response.errorMessageBlock);
+          }
+      })  
+}
--- public/loginPage.js
+++ public/loginPage.js
@@ -2,25 +2,24 @@
 const userForm = new UserForm();
 
 userForm.loginFormCallback = data => tryLogIn(data);
-
-
 function tryLogIn(data) {
-	return ApiConnector.login(data, function callback() {
-      try {
+	return ApiConnector.login(data, function callback(result) {
+      if(result.success === true) {
   	     return location.reload();
      }
-      catch { 
+      else { 
+      	return userForm.setLoginErrorMessage(result.error);
      }
   })
 }
-
-
 userForm.registerFormCallback = data => tryAuthorize(data);
 function tryAuthorize(data) {
-    return ApiConnector.register(data, function callback() {
-        try {
+    return ApiConnector.register(data, function callback(result) {
+        if (result.success === true) {
             return location.reload();
         } 
-        catch {}
+        else {
+        	return userForm.setRegisterErrorMessage(result.error);
+        }
     })
 }
